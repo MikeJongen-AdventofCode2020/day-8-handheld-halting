68db4c5fcd2e21194782f3fe302e36bb51cf7671
Mike Jongen <mikejongen@hotmail.com>
2020-12-16 16:22:26 +0100

    Initial commit

---
+++ spec/day-8-handheld-halting_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Day::8::Handheld::Halting do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/day-8-handheld-halting.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Day::8::Handheld::Halting`
+module Day::8::Handheld::Halting
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
