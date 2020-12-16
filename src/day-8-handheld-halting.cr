205b31e96933ea27e5ea5e9dd600157e29607674
Mike Jongen <mikejongen@hotmail.com>
2020-12-16 16:26:00 +0100

    Argument handling.

--- src/day-8-handheld-halting.cr
+++ src/day-8-handheld-halting.cr
@@ -1,6 +1,26 @@
-# TODO: Write documentation for `Day::8::Handheld::Halting`
-module Day::8::Handheld::Halting
-  VERSION = "0.1.0"
+require "option_parser"
+require "benchmark"
+require "string_scanner"
 
-  # TODO: Put your code here
+file_name = ""
+benchmark = false
+
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to Report Repair"
+
+  parser.on "-f FILE", "--file=FILE", "Input file" do |file|
+    file_name = file
+  end
+  parser.on "-b", "--benchmark", "Measure benchmarks" do
+    benchmark = true
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+end
+
+unless file_name.empty?
+
+
 end
